4cdb23ac8eb459ced68a19df37719490a71ca9f0
Zach Morse <zacharymorse@gmail.com>
2017-07-03 13:56:48 -0500

    basic server boilerplate, sequelize boilerplate setup

---
+++ models/users.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var users = sequelize.define('users', {
+    username: DataTypes.STRING,
+    displayname: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return users;
+};
